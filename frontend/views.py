9bbe365ae0f5c48b0cd1bf7669c4cf16b674cb9c
brice2024 <fabricenagbongou@gmail.com>
2025-04-04 18:24:46 +0000

    Projet final

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+# Create your views here.
 
 from django.shortcuts import render, redirect
 from .models import Document
 from .forms import DocumentForm
 
+
+#vue pour l'upload de fichiers
 def upload_file(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
@@ -16,6 +19,8 @@ def upload_file(request):
     
     return render(request, 'upload.html', {'form': form})
 
+
+#vue pour l'affichage de la liste des fichiers
 def file_list(request):
     documents = Document.objects.all()
     return render(request, 'file_list.html', {'documents': documents})
